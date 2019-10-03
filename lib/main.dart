2e4dee3e098bc64bd9ac3393fb8c6d07077b282d
fuser <sahil.dogstar@gmail.com>
2019-10-03 14:37:52 +0530

    Done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,74 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
+      home: DicePage(),
+    ),
+  );
+}
+
+  class DicePage extends StatefulWidget {
+    @override
+    _DicePageState createState() => _DicePageState();
+  }
+  
+  class _DicePageState extends State<DicePage> {
+
+    int leftDiceNumber = 1;
+    int rightDiceNumber = 6;
+    void modifyDice(){
+      print("Left Dice Clicked");
+      setState(() {
+        leftDiceNumber = 1 + Random().nextInt(6);
+        rightDiceNumber = 1 + Random().nextInt(6);
+      });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Roll the Dice'),
           backgroundColor: Colors.red,
         ),
-        body: Container(),
-      ),
-    ),
-  );
-}
+        body: Center(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: FlatButton(
+                  child: Image(
+                    image: AssetImage("assets/dice$leftDiceNumber.png"),
+
+                  ),
+                  onPressed: modifyDice,
+                ),
+              ),
+              Expanded(
+                  flex: 1,
+                  child: FlatButton(
+
+                    child: Image(
+                      image: AssetImage("assets/dice$rightDiceNumber.png"),
+                    ),
+                    onPressed: modifyDice,
+                  )
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: modifyDice,
+        ),
+      );
+  }
+  
+
+
+
+  }
+  
+
